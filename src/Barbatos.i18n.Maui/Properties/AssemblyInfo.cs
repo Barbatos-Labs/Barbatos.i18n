5054e278931ee14bcabf13ea3cb60f92059ffe9f
StHung <phamthehung2212000@gmail.com>
2026-07-20 02:47:37 +0700

    add XmlnsPrefix for Maui

---
+++ src/Barbatos.i18n.Maui/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Pham The Hung and Barbatos.i18n Contributors.
+// All Rights Reserved.
+
+[assembly: Microsoft.Maui.Controls.XmlnsPrefix("http://schemas.barbatos.co/i18n/2026/xaml", "i18n")]
+[assembly: Microsoft.Maui.Controls.XmlnsDefinition("http://schemas.barbatos.co/i18n/2026/xaml", "Barbatos.i18n")]
+[assembly: Microsoft.Maui.Controls.XmlnsDefinition("http://schemas.barbatos.co/i18n/2026/xaml", "Barbatos.i18n.Maui")]
